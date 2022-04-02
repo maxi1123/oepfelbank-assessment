ee40b51da3edbd96c992077f5c7d0b7270fa6d17
Maximilian Jonescu <maximilian.jonescu@uzh.ch>
2022-04-02 18:45:08 +0200

    refactor: coloring

--- src/app/components/account-card/account-card.component.ts
+++ src/app/components/account-card/account-card.component.ts
@@ -6,15 +6,18 @@ import { AccountI } from '@core/services/api/interfaces';
   selector: 'app-account-card',
   template: `
     <div [ngClass]="isTransactionView ? 'card no-margin no-hover' : 'card'">
-      <div class="logo-wrapper">
-        <app-company-logo></app-company-logo>
-      </div>
-      <h4>
-        {{ accountData.accountSubType }}
-      </h4>
-      <div style="display: flex; align-items: center">
-        <h2 [ngClass]="accountData.balance < 0 ? 'alert-negative' : ''">{{ accountData.balance }}</h2>
-        <h4 class="currency">{{ accountData.currency }}</h4>
+      <div class="top-bar"></div>
+      <div class="body-wrapper">
+        <div class="logo-wrapper">
+          <app-company-logo></app-company-logo>
+        </div>
+        <h4>
+          {{ accountData.accountSubType }}
+        </h4>
+        <div style="display: flex; align-items: center">
+          <h2 [ngClass]="accountData.balance < 0 ? 'alert-negative' : ''">{{ accountData.balance }}</h2>
+          <h4 class="currency">{{ accountData.currency }}</h4>
+        </div>
       </div>
     </div>
   `,
